40e1f7c4e29aa9ea9f2ce2c480ff2d7db167c9c2
burcugurel <burcugurel_2@hotmail.com>
2017-06-20 23:35:51 +0300

    Video 10

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var car = {
+	make: "VW",
+	type: "Polo",
+	color: "blue",
+	isTurnedOn: false, //boolean
+	numberOfWheel: 4,
+	seats: 
+	[
+	"saet1",
+	"seat2", 
+	"seat3", 
+	"seat4"
+	],
+
+	turnOn: function() {
+		this.isTurnedOn = true;
+	}
+	fly: function() {
+		alert("fly");
+	},
+
+	switchCar: function (isOn) {
+		console.log("turn car" +isOn);
+		if(isOn == true) {
+			this.isTurnedOn = true;
+		}
+		else {
+			this.isTurnedOn = false;
+		}
+
+	}
+};
+
+
+	console.log('hello there friends! ')
+
